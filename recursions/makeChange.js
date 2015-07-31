4585eae0819b20245429954f2998e3bffe196af4
= <qg231@nyu.edu>
2015-07-30 21:48:25 -0700

    Write makeChange

---
+++ recursions/makeChange.js
@@ -0,0 +1,32 @@
+(function(){
+  if(typeof Recursion === "undefined"){
+    Recursion = {};
+  }
+
+  var makeChange = Recursion.makeChange = function(target, denominations){
+    if(target === 0 || denominations.length === 0){ return []; }
+
+    var firstCoin = denominations[0];
+    var secondCoin = denominations[1];
+
+    if(target >= firstCoin){
+      if(secondCoin === undefined){
+        return makeChange(target - firstCoin, denominations).concat(firstCoin);
+      }else{
+        var firstTry = makeChange(target - firstCoin, denominations).concat(firstCoin);
+        var secondTry = makeChange(target- secondCoin, denominations).concat(secondCoin);
+
+        if(firstTry.length > secondTry.length){
+          return secondTry;
+        }else{
+          return firstTry;
+        }
+      }
+    }else{
+      // The biggest denomination doesn't fit, so slice if off and try again.
+      return makeChange(target, denominations.slice(1));
+    }
+  }
+
+  console.log(makeChange(47, [8, 7, 3, 1]));
+})();
