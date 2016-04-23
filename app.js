13028e7cb0b9f3ab48e844e1141188992b19665d
= <qg231@nyu.edu>
2016-04-23 16:12:13 -0400

    Add reverse linked list

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-var Dynamic = require('./dynamic-programming/frogCrossing');
+var DataStructure = require('./data-structures/linkedList.js');
 
-console.log(Dynamic.frogCrossing(10, [1, 2, 4]));
+var Node = DataStructure.LinkedListNode;
+var node5 = new Node({ val: 5 });
+var node4 = new Node({ val: 30 });
+var node3 = new Node({ val: 4 });
+var node2 = new Node({ val: 30 });
+var node1 = new Node({ val: 198 });
+var node0 = new Node({ val: 99 });
+
+var linkedList = new DataStructure.LinkedList({ head: node0 });
+linkedList.append(node1);
+linkedList.append(node2);
+linkedList.append(node3);
+linkedList.append(node4);
+linkedList.append(node5);
+
+linkedList.print();
+
+linkedList.reverse();
+linkedList.print();
--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -1,8 +1,68 @@
+if(typeof DataStructure == "undefined"){
+  var DataStructure = {};
+}
+
 var LinkedListNode = function(options){
   this.val = options.val;
   this.next = options.next;
 };
 
+LinkedListNode.prototype.toString = function(){
+  return "{ value: " + this.val + "; next: " + (this.next? "[node]": "NULL") + "}";
+}
+
 var LinkedList = function(options){
   this.head = options.head;
+
+  // Find last
+  var node = this.head;
+  while(node.next){
+    node = node.next;
+  }
+  this.last = node;
 }
+
+LinkedList.prototype.reverse = function(){
+  if(!this.head || !this.head.next){
+    return this;
+  }
+
+  var current = this.head;
+  var pre = null;
+  var post = null;
+
+  while(current){
+    post = current.next;
+    current.next = pre;
+    pre = current;
+    current = post;
+  }
+
+  this.head = pre;
+  return this;
+}
+
+LinkedList.prototype.append = function(newNode){
+  this.last.next = newNode;
+  this.last = newNode;
+}
+
+LinkedList.prototype.print = function(){
+  var node = this.head;
+  var result = "";
+
+  while(node){
+    result += node.toString();
+    node = node.next;
+    if(node){ result += " => "; }
+  }
+
+  console.log(result);
+
+  return 0;
+}
+
+DataStructure.LinkedListNode = LinkedListNode;
+DataStructure.LinkedList = LinkedList;
+
+module.exports = DataStructure;
